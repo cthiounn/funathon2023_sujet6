117f51932a73c7a783bc2ecb7b5a6442f0784b40
cthiounn2 <thiounn.conrad@gmail.com>
2023-07-04 12:20:44 +0000

    yannis model by default

--- streamlit-api.py
+++ streamlit-api.py
@@ -3,7 +3,12 @@ import spacy_streamlit
 
 
 st.text("Version : 1.0 ")
+st.markdown('# Darespresso')
+st.divider()
+st.info('Prenez le café avec Proust', icon="ℹ️")
+st.divider()
 
-models = ["ner_miam_spacy_nlp","model-best"]
+models = ["model-best","ner_miam_spacy_nlp"]
 default_text = "La madeleine de Proust."
-spacy_streamlit.visualize(models, default_text)
+visualizers = ["ner"]
+spacy_streamlit.visualize(models, default_text, visualizers)
